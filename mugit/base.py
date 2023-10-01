ee0936212e93fdc96048e09dad4df26783f523de
lebbion <145786435+lebbion@users.noreply.github.com>
2023-10-01 13:50:10 +0300

    write-tree: ignore .ugit files

--- mugit/base.py
+++ mugit/base.py
@@ -7,9 +7,17 @@ def write_tree(directory="."):
     with os.scandir(directory) as it:
         for entry in it:
             full = f"{directory}/{entry.name}"
+
+            if is_ignored(full):
+                continue
+
             if entry.is_file(follow_symlinks=False):
                 # TODO write the file to object store
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
     # TODO create the tree object
+
+
+def is_ignored(path):
+    return data.GIT_DIR in path.split("/")
